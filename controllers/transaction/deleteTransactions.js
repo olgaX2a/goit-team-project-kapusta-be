b131a326fa1af79c294ee7ee6a99a78cf3d34b8e
Yaroslav Prokudin <jaroslav.prokudin@gmail.com>
2021-11-10 10:57:08 +0200

    added delete transactions

--- controllers/transaction/deleteTransactions.js
+++ controllers/transaction/deleteTransactions.js
@@ -1,15 +1,11 @@
-const { User } = require("../../model/user.js");
 const { Transaction } = require("../../model/index");
 const { NotFound } = require('http-errors');
 
 
 const deleteTransactions = async (req, res) => {
   const { _id } = req.user;
-  // console.log(req.user)
   const { transactionId } = req.params;
-  // console.log(transactionId)
-  const transaction = await Transaction.findOneAndRemove({ _id: transactionId, owner: user._id});
-  console.log(transaction)
+  const transaction = await Transaction.findOneAndRemove({ _id: transactionId, owner: _id});
   if (!transaction) {
     throw new NotFound(`Transaction with id=${transactionId} is not found`);
   }
