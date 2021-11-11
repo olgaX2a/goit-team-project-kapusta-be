026945d09bd2be07cc8b73490328397f9269a75d
Denys Novoidarskyi <dreamerlife@ukr.net>
2021-11-11 20:33:18 +0200

    добавлен возврат имени и почты на фронт

--- controllers/user/login.js
+++ controllers/user/login.js
@@ -1,26 +1,23 @@
-const { User } = require("../../model/user.js");
-const { BadRequest, NotFound } = require("http-errors");
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs");
+const { User } = require('../../model/user.js');
+const { BadRequest, NotFound } = require('http-errors');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 const { SECRET_KEY } = process.env;
 
 const login = async (req, res) => {
   const { name, email, password } = req.body;
-  const user = await User.findOne(
-    { email },
-    "_id name email balance password verify"
-  );
+  const user = await User.findOne({ email }, '_id name email balance password verify');
   if (!user) {
     throw new NotFound(`Email ${email} not found`);
   }
   if (!bcrypt.compareSync(password, user.password)) {
-    throw new BadRequest("Invalid password");
+    throw new BadRequest('Invalid password');
   }
   if (!user.verify) {
-    throw new BadRequest("Email not verify");
+    throw new BadRequest('Email not verify');
   }
-  const { _id, balance } = user;
+  const { _id, balance, name: userName, email: userEmail } = user;
 
   const payload = {
     _id,
@@ -28,10 +25,11 @@ const login = async (req, res) => {
   const token = jwt.sign(payload, SECRET_KEY);
   await User.findByIdAndUpdate(_id, { token });
   res.json({
-    status: "success",
+    status: 'success',
     code: 200,
     data: {
-      name,
+      userName,
+      userEmail,
       token,
       balance: balance.toFixed(2),
     },
