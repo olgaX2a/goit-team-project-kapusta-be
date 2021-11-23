6383a5bf752af1acfa0cb68d237e204f5defa4d0
Denys Novoidarskyi <dreamerlife@ukr.net>
2021-11-23 02:30:36 +0200

    выполнена оптимизация кода

--- controllers/transaction/getTransactions.js
+++ controllers/transaction/getTransactions.js
@@ -5,17 +5,15 @@ const getTransactions = async (req, res) => {
   const { _id, balance } = req.user;
   const { transactionType } = req.params;
 
-  const transactionsById = await transactionFind({ owner: _id });
-  const result = transactionsById.filter(
-    transaction => transaction.transactionType === transactionType,
-  );
-  const totalAmountByMonth = getTotalAmountByMonth(result);
+  const transactionsById = await transactionFind({ owner: _id, transactionType });
+
+  const totalAmountByMonth = await getTotalAmountByMonth(transactionsById);
   res.status(201).json({
     status: 'success',
     code: 201,
     balance,
     totalAmountByMonth,
-    data: result,
+    data: transactionsById,
   });
 };
 
